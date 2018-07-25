a64c37ed46a339a651eea24c64ca3a9d2e10d0cb
zhyj0215 <12345>
2018-07-25 20:36:22 +0800

    完成首页组渲染

--- src/main.js
+++ src/main.js
@@ -4,19 +4,19 @@
 import Vue from 'vue' //相当于Node中 var Vue = require('vue')
 import App from './App.vue';
 // 引入路由模块
-// import VueRouter from 'vue-router';
+import VueRouter from 'vue-router';
 // //引入index 组件
-// import index from "./components/index.vue";
+import index from "./components/index.vue";
 //中间件
 Vue.use(VueRouter);
 //实例化路由
-// const router = new VueRouter({
-//     routes :[
-//         { path: '/', component: index },
-//       { path: '/index', component: index }
+const router = new VueRouter({
+    routes :[
+        { path: '/', component: index },
+      { path: '/index', component: index }
     
-//     ]
-//   })
+    ]
+  })
   
 new Vue({
     el:"#app",
@@ -25,6 +25,6 @@ new Vue({
     //     return createElement(App)
     // } 
     render:h=>h(App),
-    // router
+     router
   
 })
